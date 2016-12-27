d32092b9e127ce96416922ab2bf5ba9df897d779
namwoody <deepsarora262@gmail.com>
2016-12-26 22:05:53 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'cozumel');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'D7Hpk2lRsx');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
